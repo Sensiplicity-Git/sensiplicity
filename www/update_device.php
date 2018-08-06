4408afea22c0ae16f3d8c7466a28645dbb219fa1
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-06 18:29:19 +0000

    Removed extra update message from update page

--- www/update_device.php
+++ www/update_device.php
@@ -12,7 +12,6 @@ $value = isset($login_session) ? $login_session : '';
 <br>
   <table align="center" border=1 cellpadding='5' cellspacing='5'>
    <tr><td width="90%">
-<<<<<<< HEAD
 	<h2>Update Status:</h2>
    </td>
    <td><h3> <?php print `/opt/sensiplicity/bin/git_status.sh` ?> </h3></td>
@@ -24,15 +23,3 @@ $value = isset($login_session) ? $login_session : '';
   </table>
 <?php require("footer.php"); ?>  
 
-=======
-        <h2>Update Status:</h2>
-   </td>
-   <td><h3> <?php print `/opt/sensiplicity/bin/git_status.sh` ?> </h3></td>
-   <td width="10%">
-        <form id="update_device" name="update_device" action="adsfasdfadsfasdfasdfasdf.php" method="get">
-        <input type="submit" name='UpdateDevice' value="Update The System Device" />
-        </form>
-   </td></tr>
-  </table>
-<?php require("footer.php"); ?>
->>>>>>> 04705645ce82d7d51f1d78c0c4c8f10f4b655290
