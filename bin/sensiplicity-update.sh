ddca9318fb578547d7ac165abad551fd8dcac747
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-07 21:17:58 +0000

    Added file switching lines--Eg, swaps update to sensiplicity

--- bin/sensiplicity-update.sh
+++ bin/sensiplicity-update.sh
@@ -2,6 +2,10 @@
 if [ -e /opt/sensiplicity/update_set.up ]
 then
 	echo "Attempting Update..."
+	/bin/mv /opt/sensiplicity/etc/*.conf /opt/update/etc
+	/bin/rm -rf /opt/old
+	/bin/mv -f /opt/sensiplicity /opt/old 
+	/bin/mv -f /opt/update /opt/sensiplicity
 else
 	echo "Not Attempting Update"
 fi
