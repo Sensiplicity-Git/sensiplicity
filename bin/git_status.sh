66421985acfa837df433f0ec2a746bec4d1b57ae
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-07 22:16:09 +0000

    Added updating remote status

--- bin/git_status.sh
+++ bin/git_status.sh
@@ -5,10 +5,12 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
+git remote update
+
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    echo "Need to update"
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
