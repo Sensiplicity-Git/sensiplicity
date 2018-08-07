5bc85e1084cd0ff0b77dd1e63fac7da50524bb17
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-07 20:42:09 +0000

    Modified set_update.php to clone the git repsitory into an update folder

--- bin/sensiplicity-update.sh
+++ bin/sensiplicity-update.sh
@@ -2,8 +2,6 @@
 if [ -e /opt/sensiplicity/update_set.up ]
 then
 	echo "Attempting Update..."
-	/usr/bin/git -C /opt/sensiplicity pull
-	/bin/rm /opt/sensiplicity/update_set.up
 else
 	echo "Not Attempting Update"
 fi
--- www/set_update.php
+++ www/set_update.php
@@ -8,6 +8,7 @@
 
 <?php require("header_admin.php"); ?>
 <?php `sudo sh -c 'echo "1" > ../update_set.up'`; ?>
+<?php echo `sudo git clone https://github.com/Sensiplicity-Git/sensiplicity.git /opt/update`; ?>
 <h3><a href="admin.php">Back To Admin Page</a> </h3>
 <br>
 <br>
