db156f17576e649d59d06677bea9c9b2d5cf8190
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-09 18:21:22 +0000

    Modified to set sticky bits

--- bin/sensiplicity-update.sh
+++ bin/sensiplicity-update.sh
@@ -6,6 +6,10 @@ then
 	/bin/rm -rf /opt/old
 	/bin/mv -f /opt/sensiplicity /opt/old 
 	/bin/mv -f /opt/update /opt/sensiplicity
+	chmod 4755 /opt/sensiplicity/bin/alarm-switch-daemon
+	chmod 4755 /opt/sensiplicity/bin/sn-util-pi
+	chmod 4755 /opt/sensiplicity/bin/halt_system
+
 else
 	echo "Not Attempting Update"
 fi
