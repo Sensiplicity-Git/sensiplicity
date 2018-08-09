deb621091f1814490e682db580ada22b67998ea7
root <root@jaiswal-sp2.cgrb.oregonstate.edu>
2018-08-09 18:29:40 +0000

    Added comments

--- bin/sensiplicity-update.sh
+++ bin/sensiplicity-update.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+
+#Check to see if update flag file exists
 if [ -e /opt/sensiplicity/update_set.up ]
 then
 	echo "Attempting Update..."
+	#Move config file into updated folder
 	/bin/mv /opt/sensiplicity/etc/*.conf /opt/update/etc
+	#Remove old Sensiplcity folder
 	/bin/rm -rf /opt/old
+	#Make current Sensiplicity folder old 
 	/bin/mv -f /opt/sensiplicity /opt/old 
+	#Make update folder the current running Sensiplicity version
 	/bin/mv -f /opt/update /opt/sensiplicity
+	#Set sticky bits so that these can be run as root
 	chmod 4755 /opt/sensiplicity/bin/alarm-switch-daemon
 	chmod 4755 /opt/sensiplicity/bin/sn-util-pi
 	chmod 4755 /opt/sensiplicity/bin/halt_system
