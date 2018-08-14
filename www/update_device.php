17264f9690833e4fc0fe3f00a0f70f95b615c18f
Sensiplicity <41711084+Sensiplicity@users.noreply.github.com>
2018-08-14 17:01:23 +0000

    Update update_device.php

--- www/update_device.php
+++ www/update_device.php
@@ -14,11 +14,11 @@ $value = isset($login_session) ? $login_session : '';
 <br>
 <br>
   <table align="center" border=1 cellpadding='5' cellspacing='5'>
-   <tr><td width="80%">
+   <tr><td width="70%">
 	<h1>Update Status:</h1>
 	<h2>If the system needs to be updated the button to the right will become active. You can click the button and the system will pull the update from online after which you will need to reboot the system. Since the data is located online the device needs to be connected to the network to be updated.</h2>
    </td>
-   <td width="10%"><h3> <?php print `/opt/sensiplicity/bin/git_status.sh` ?> </h3></td>
+   <td width="20%"><h3> <?php print `/opt/sensiplicity/bin/git_status.sh` ?> </h3></td>
    <td width="10%">
 	<form id="update_device" name="update_device" action="set_update.php" method="get">
 	<input <?php if ($status == "Up-to-date\n") { echo " disabled='disabled'";}  ?> type="submit" name='UpdateDevice' value='Update Device' />
